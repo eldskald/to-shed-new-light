471e0bc34563b8cff3a19cf416a3756b294946c5
eldskald <rafaelbordoni00@gmail.com>
2021-10-21 16:00:40 -0300

    Update drag controls code

--- source/assets/scripts/controls-drag.gd
+++ source/assets/scripts/controls-drag.gd
@@ -3,34 +3,36 @@ extends RayCast
 onready var sphere: Spatial = null
 onready var path: Spatial = null
 
+signal on_grab_sphere
+signal on_let_go_of_sphere
+
 func _physics_process(_delta: float) -> void:
 	update_raycast_position()
+	force_raycast_update()
 	
-	if Input.is_action_pressed("click"):
-		
-		# First, let's try to grab the sphere.
-		force_raycast_update()
+	# First, let's try to grab the sphere.
+	if Input.is_action_just_pressed("click"):
 		if sphere == null:
-			force_raycast_update()
 			if self.is_colliding():
 				if get_collider().is_in_group("main_sphere"):
 					sphere = get_collider() # Remember sphere for next frame.
 					add_exception(sphere)
+					emit_signal("on_grab_sphere")
 					force_raycast_update()
-		
-		# Now, if there is a sphere grabbed, let's search for paths.
-		if sphere != null:
-			if self.is_colliding():
-				if get_collider().is_in_group("path"):
-					path = get_collider() # Remember path for next frame.
-					drag_sphere(get_collision_point())
-				
-				# In case we don't find a path, we use last frame's path
-				# value to snap the sphere to its center.
-				elif path != null:
-					let_go_of_sphere()
+	
+	# Now, if there is a sphere grabbed, let's search for paths.
+	if Input.is_action_pressed("click") and sphere != null:
+		if self.is_colliding():
+			if get_collider().is_in_group("path"):
+				path = get_collider() # Remember path for next frame.
+				drag_sphere(get_collision_point())
+			
+			# In case we don't find a path, we use last frame's path
+			# value to snap the sphere to its center.
 			elif path != null:
 				let_go_of_sphere()
+		elif path != null:
+			let_go_of_sphere()
 	
 	# Now, letting go of the sphere by letting go of the mouse button.
 	if Input.is_action_just_released("click"):
@@ -59,4 +61,8 @@ func let_go_of_sphere() -> void:
 	sphere = null
 	path = null
 	clear_exceptions()
+	emit_signal("on_let_go_of_sphere")
+
+func is_grabbing_sphere() -> bool:
+	return sphere != null
 
--- source/assets/scripts/scene-message.gd
+++ source/assets/scripts/scene-message.gd
@@ -1,11 +1,30 @@
 extends Control
 
 export(float) var auto_activate_in
+export(float) var deactivate_after
 
 onready var timer = $Timer
 onready var anim_player = $AnimationPlayer
 
 signal on_activate
+signal on_deactivate
 
-func _ready():
-	pass
+func _ready() -> void:
+	if auto_activate_in > 0:
+		timer.start(auto_activate_in)
+
+func _on_timer_timeout() -> void:
+	if self.modulate.a == 0:
+		activate()
+	else:
+		deactivate()
+
+func activate() -> void:
+	anim_player.play("activate")
+	emit_signal("on_activate")
+	if deactivate_after > 0:
+		timer.start(deactivate_after)
+
+func deactivate() -> void:
+	anim_player.play("deactivate")
+	emit_signal("on_deactivate")
