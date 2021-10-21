267b9fe0aa8a09878259601c8d92ef0809fe5eb1
eldskald <rafaelbordoni00@gmail.com>
2021-10-21 18:03:08 -0300

    Update drag controls code

--- source/assets/scripts/controls-drag.gd
+++ source/assets/scripts/controls-drag.gd
@@ -8,36 +8,33 @@ signal on_let_go_of_sphere
 
 func _physics_process(_delta: float) -> void:
 	update_raycast_position()
-	force_raycast_update()
 	
-	# First, let's try to grab the sphere.
+	# First, let's check if the player grabbed the sphere.
 	if Input.is_action_just_pressed("click"):
-		if sphere == null:
-			if self.is_colliding():
-				if get_collider().is_in_group("main_sphere"):
-					sphere = get_collider() # Remember sphere for next frame.
-					add_exception(sphere)
-					emit_signal("on_grab_sphere")
-					force_raycast_update()
-	
-	# Now, if there is a sphere grabbed, let's search for paths.
-	if Input.is_action_pressed("click") and sphere != null:
-		if self.is_colliding():
-			if get_collider().is_in_group("path"):
-				path = get_collider() # Remember path for next frame.
+		sphere = find_sphere()
+		if sphere != null:
+			add_exception(sphere)
+			path = find_path()
+			if path != null:
+				emit_signal("on_grab_sphere")
 				drag_sphere(get_collision_point())
-			
-			# In case we don't find a path, we use last frame's path
-			# value to snap the sphere to its center.
-			elif path != null:
-				let_go_of_sphere()
-		elif path != null:
-			let_go_of_sphere()
+			else:
+				sphere = null
+				clear_exceptions()
 	
-	# Now, letting go of the sphere by letting go of the mouse button.
-	if Input.is_action_just_released("click"):
-		if sphere != null and path != null:
-			let_go_of_sphere()
+	# If there is a sphere, either from this frame or from a previous
+	# one, let's now check to see if the player is dragging it.
+	elif Input.is_action_pressed("click") and sphere != null:
+		var new_path = find_path() # In order to remember last frame's path.
+		if new_path != null:
+			path = new_path
+			drag_sphere(get_collision_point())
+		else:
+			let_go_of_sphere() # Snap sphere to last frame's path center.
+	
+	# Lastly, to check if the player had just let go of the sphere.
+	elif Input.is_action_just_released("click") and sphere != null:
+		let_go_of_sphere() # Snap sphere to current frame's path center.
 
 
 
@@ -51,6 +48,20 @@ func update_raycast_position() -> void:
 	self.translation.x = unit_pos.x * camera.size / 2
 	self.translation.y = -unit_pos.y * camera.size / 2
 	self.translation.y *= viewport.size.y / viewport.size.x
+
+func find_sphere() -> Object:
+	force_raycast_update()
+	if self.is_colliding():
+		if get_collider().is_in_group("main_sphere"):
+			return get_collider()
+	return null
+
+func find_path() -> Object:
+	force_raycast_update()
+	if self.is_colliding():
+		if get_collider().is_in_group("path"):
+			return get_collider()
+	return null
 
 func drag_sphere(new_position: Vector3) -> void:
 	var offset: Vector3 = new_position - sphere.to_global(Vector3.ZERO)
