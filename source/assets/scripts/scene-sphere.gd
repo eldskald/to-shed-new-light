7a25307f34cbff8506a63ba2bd2429fb313ea087
eldskald <rafaelbordoni00@gmail.com>
2021-10-21 23:06:40 -0300

    Make the sphere brighter when grabbed

--- source/assets/scripts/scene-sphere.gd
+++ source/assets/scripts/scene-sphere.gd
@@ -1,5 +1,6 @@
 extends Spatial
 
+onready var material: Material = $MeshInstance.get_surface_material(0)
 onready var tween: Tween = $Tween
 onready var sphere_grab_sfx: AudioStreamPlayer = $SphereGrab
 onready var sphere_grab_sfx_tween: Tween = $SphereGrab/Tween
@@ -8,6 +9,8 @@ var reparent_call: Spatial = null
 signal stage_cleared
 
 func _physics_process(_delta):
+	var lin_volume: float = inverse_lerp(-80, -4, sphere_grab_sfx.volume_db)
+	material.set_shader_param("brightness", lerp(0.8, 1.1, lin_volume))
 	if reparent_call != null:       # Can't reparent on an area_entered signal
 		reparent_to(reparent_call)  # or else the game crashes, so we're doing
 		reparent_call = null        # it on the next frame instead.
