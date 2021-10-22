ed36e1dbce1a4281cae8ea2664116cccf3e50cc8
Ayrton <makkun1232@gmail.com>
2021-10-22 16:05:02 -0300

    ADD Dynamic brightness

--- source/assets/scripts/scene-sphere.gd
+++ source/assets/scripts/scene-sphere.gd
@@ -4,13 +4,19 @@ onready var material: Material = $MeshInstance.get_surface_material(0)
 onready var tween: Tween = $Tween
 onready var sphere_grab_sfx: AudioStreamPlayer = $SphereGrab
 onready var sphere_grab_sfx_tween: Tween = $SphereGrab/Tween
+onready var sfx_spectrum:AudioEffectSpectrumAnalyzerInstance = AudioServer.get_bus_effect_instance(
+																AudioServer.get_bus_index("SFX"), 0)
 
 var reparent_call: Spatial = null
 signal stage_cleared
 
 func _physics_process(_delta):
-	var lin_volume: float = inverse_lerp(-80, -4, sphere_grab_sfx.volume_db)
+	var sfx_mag :Vector2 = sfx_spectrum.get_magnitude_for_frequency_range(400, 2000)
+	var lin_volume = sfx_mag.length()*4 + 0.8
+	print(lin_volume)
 	material.set_shader_param("brightness", lerp(0.8, 1.2, lin_volume))
+	#var lin_volume: float = inverse_lerp(-80, -4, sphere_grab_sfx.volume_db)
+	#material.set_shader_param("brightness", lerp(0.8, 1.2, lin_volume))
 	if reparent_call != null:       # Can't reparent on an area_entered signal
 		reparent_to(reparent_call)  # or else the game crashes, so we're doing
 		reparent_call = null        # it on the next frame instead.
