831bfd5ab3d3fa9e3920960d5baaa6ca7eda5dfb
eldskald <rafaelbordoni00@gmail.com>
2021-10-23 18:07:05 -0300

    Update widget model

--- source/assets/scripts/scene_pivot_widget.gd
+++ source/assets/scripts/scene_pivot_widget.gd
@@ -1,7 +1,6 @@
 extends Area
 
-onready var material_1: Material = $MeshInstance.get_surface_material(0)
-onready var material_2: Material = $MeshInstance.get_surface_material(1)
+onready var mesh: MeshInstance = $MeshInstance
 onready var timer: Timer = $Timer
 
 var brightness: float = 0.6
@@ -10,10 +9,9 @@ var alpha: float = 1
 func _process(_delta) -> void:
 	if not timer.is_stopped():
 		alpha = 1 - timer.time_left
-	material_1.set_shader_param("brightness", brightness)
-	material_2.set_shader_param("brightness", brightness)
-	material_1.set_shader_param("alpha", alpha)
-	material_2.set_shader_param("alpha", alpha)
+	for i in range(mesh.get_surface_material_count()):
+		mesh.get_surface_material(i).set_shader_param("brightness", brightness)
+		mesh.get_surface_material(i).set_shader_param("alpha", alpha)
 
 func _on_Timer_timeout() -> void:
 	$CollisionShape.disabled = false
