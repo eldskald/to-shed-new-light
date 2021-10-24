4ec047690bf899b97511c6930617a1296038d2e5
Ayrton <makkun1232@gmail.com>
2021-10-24 13:49:51 -0300

    Integrate Ui SFX

--- source/assets/scripts/ui-starting-screen.gd
+++ source/assets/scripts/ui-starting-screen.gd
@@ -11,9 +11,11 @@ func _on_Transition_fade_out_finished():
 	GameStateMachine.set_state(GameStateMachine.states.GAME)
 
 func _on_StartGame_pressed():
+	$SelectSFX.play()
 	transition.fade_out()
 
 func _on_Credits_pressed():
+	$SelectSFX.play()
 	anim_player.play("load_credits")
 
 func _input(event: InputEvent) -> void:
@@ -21,6 +23,7 @@ func _input(event: InputEvent) -> void:
 		if $Credits.visible and !anim_player.is_playing():
 			anim_player.play("back_to_menu")
 		elif anim_player.is_playing():
+			$SelectSFX.play()
 			var default_speed = anim_player.playback_speed
 			anim_player.playback_speed = 9999
 			yield(anim_player, "animation_finished")
