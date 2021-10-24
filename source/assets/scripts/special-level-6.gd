5bed788635b8d933c7e12b2d3b0eb17ad5452a54
eldskald <rafaelbordoni00@gmail.com>
2021-10-24 19:52:56 -0300

    Update level 6

--- source/assets/scripts/special-level-6.gd
+++ source/assets/scripts/special-level-6.gd
@@ -2,12 +2,18 @@ extends Timer
 
 onready var level: Spatial = get_parent()
 
+var second_timer: bool = false
+
 signal changed_direction
 
 func _on_timeout():
-	level.activate_widget("Z")
-	level.activate_widget("Y")
+	if not second_timer:
+		level.activate_widget("Z")
+		level.activate_widget("Y")
+	else:
+		emit_signal("changed_direction")
 
 func _on_CameraControls_changed_view_direction(new_direction: String):
 	if new_direction == "Y":
-		emit_signal("changed_direction")
+		self.start(1)
+		second_timer = true
