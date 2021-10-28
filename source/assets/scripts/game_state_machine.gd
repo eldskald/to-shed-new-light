6db2383328908496735a36bc75660b8f8f734285
eldskald <rafaelbordoni00@gmail.com>
2021-10-28 19:37:34 -0300

    Add reset button

--- source/assets/scripts/game_state_machine.gd
+++ source/assets/scripts/game_state_machine.gd
@@ -2,7 +2,7 @@ extends Node
 
 enum states {START_MENU, GAME, CREDITS}
 
-export(String, DIR) var levels_derectory
+export(String, DIR) var levels_directory
 export(String) var level_prefix
 export(String, FILE, "*.tscn") var start_menu
 export(String, FILE, "*.tscn") var credits
@@ -18,11 +18,9 @@ func set_state(new_value: int) -> void:
 	match new_value:
 		states.START_MENU:
 			get_tree().change_scene(start_menu)
-			level_progress = 1
 		states.GAME:
-			get_tree().change_scene(
-				levels_derectory + "/" + level_prefix +
-				String(level_progress) + ".tscn")
+			level_progress = 1
+			go_to_level(level_progress)
 		states.CREDITS:
 			get_tree().change_scene(credits)
 	state = new_value
@@ -33,15 +31,20 @@ func _ready():
 func stage_cleared():
 	if self.state != states.GAME:
 		return
-	
 	level_progress += 1
-	var level_file: String = levels_derectory + "/"
-	level_file += level_prefix + String(level_progress) + ".tscn"
+	go_to_level(level_progress)
+
+func reset_level():
+	if self.state != states.GAME:
+		return
+	go_to_level(level_progress)
+
+func go_to_level(level: int) -> void:
+	var level_file: String = levels_directory + "/"
+	level_file += level_prefix + String(level) + ".tscn"
 	var file_checker := File.new()
 	if file_checker.file_exists(level_file):
-		get_tree().change_scene(
-			levels_derectory + "/" + level_prefix +
-			String(level_progress) + ".tscn")
+		get_tree().change_scene(level_file)
 	else:
 		print("Game ended")
 		get_tree().quit()
--- source/assets/scripts/ui-debug.gd
+++ source/assets/scripts/ui-debug.gd
@@ -6,3 +6,6 @@ onready var fps_label: Label = $FPS
 func _process(_delta):
 	level_label.text = "Level " + GameStateMachine.level_progress as String
 	fps_label.text = Engine.get_frames_per_second() as String + " fps"
+
+func _on_ResetButton_pressed():
+	GameStateMachine.reset_level()
