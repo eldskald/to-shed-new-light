92786b4d4e4405c838cc9391c0b534e7b6c2a851
eldskald <rafaelbordoni00@gmail.com>
2021-10-29 04:48:12 -0300

    Added a mouse flick limit to fix the flick exploit

--- source/assets/scripts/controls-drag.gd
+++ source/assets/scripts/controls-drag.gd
@@ -3,6 +3,8 @@ extends RayCast
 onready var sphere: Spatial = null
 onready var path: Spatial = null
 
+var last_mouse_pos: Vector2 = Vector2.ZERO
+
 signal grabbed_sphere
 signal let_go_of_sphere
 
@@ -28,24 +30,41 @@ func _physics_process(_delta: float) -> void:
 		var new_path = find_path() # In order to remember last frame's path.
 		if new_path != null:
 			path = new_path
-			sphere.global_transform.origin = get_collision_point()
+			var new_mouse_pos: Vector2 = get_mouse_position_in_3d_units()
+			if (new_mouse_pos - last_mouse_pos).length() < 0.5:
+				sphere.global_transform.origin = get_collision_point()
+			else:
+				let_go_of_sphere()
 		else:
 			let_go_of_sphere() # Snap sphere to last frame's path center.
 	
-	# Lastly, to check if the player had just let go of the sphere.
+	# Check if the player had just let go of the sphere.
 	elif Input.is_action_just_released("click") and sphere != null:
 		let_go_of_sphere() # Snap sphere to current frame's path center.
+	
+	# Lastly, we record the mouse position. We need it on the dragging part
+	# of the code to prevent the player from flicking the mouse too quick,
+	# which could allow the sphere to teleport long distances from a valid
+	# position to another valid position which have a barrier or a gap in
+	# between them, so we have to force the player to move slower.
+	last_mouse_pos = get_mouse_position_in_3d_units()
 
-func update_raycast_position() -> void:
+func get_mouse_position_in_3d_units() -> Vector2:
 	var viewport: Viewport = get_viewport()
 	var camera: Camera = viewport.get_camera()
 	var mouse_position: Vector2 = viewport.get_mouse_position()
 	var unit_pos: Vector2 = Vector2(                 # Makes mouse pos coords
 		2 * mouse_position.x / viewport.size.x - 1,  # range from -1 to 1 with
 		2 * mouse_position.y / viewport.size.y - 1)  # (0,0) on screen center.
-	self.translation.x = unit_pos.x * camera.size / 2
-	self.translation.y = -unit_pos.y * camera.size / 2
-	self.translation.y *= viewport.size.y / viewport.size.x
+	var r: Vector2 = Vector2.ZERO
+	r.x = unit_pos.x * camera.size / 2
+	r.y = -unit_pos.y * camera.size * viewport.size.y / (2 * viewport.size.x)
+	return r
+
+func update_raycast_position() -> void:
+	var coords: Vector2 = get_mouse_position_in_3d_units()
+	self.translation.x = coords.x
+	self.translation.y = coords.y
 
 func find_sphere() -> Object:
 	force_raycast_update()
