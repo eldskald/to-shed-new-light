abfc4e0ad97ca75de44258b12706da966585a6fc
eldskald <rafaelbordoni00@gmail.com>
2021-10-29 22:53:18 -0300

    Update GUI settings

--- source/assets/scripts/ui-debug.gd
+++ source/assets/scripts/ui-debug.gd
@@ -3,6 +3,10 @@ extends VBoxContainer
 onready var level_label: Label = $Level
 onready var fps_label: Label = $FPS
 
+func _ready():
+	if GameStateMachine.state != GameStateMachine.states.GAME:
+		$ResetButton.visible = false
+
 func _process(_delta):
 	level_label.text = "Level " + GameStateMachine.level_progress as String
 	fps_label.text = Engine.get_frames_per_second() as String + " fps"
